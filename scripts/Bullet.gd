924004e5a4e765379c21a06f0a763692b6e19578
sam <watcharapong.pstst@gmail.com>
2021-03-08 23:04:43 +0700

    [ADD] Bullet Capsule, Enemies Color Random

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -4,12 +4,4 @@ extends RigidBody2D
 func _ready():
 	yield(get_tree().create_timer(1),"timeout")
 	if(is_instance_valid(get_tree())):
-		queue_free()
-
-func _on_Area2D_body_entered(body):
-	var damageFunction = get_node("/root/DamageFunction")
-	if body.is_in_group("Enemy"):
-		body.damage(damageFunction.damageCalculate())
-		#body.call_deferred("queue_free")
 		call_deferred("queue_free")
-	#rotation = rotation.reflect(ray.get_collision_normal())
--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
 const speed = 200
-export var maxHp = 300
+var randomMaxHpRange = [0, 500]
+var maxHp = null
 var target = null
 var hp = null
 onready var sprite = $Sprite
@@ -10,7 +11,10 @@ onready var healthBar = $HealthBar
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	healthBar.value = 100
+	maxHp = rand_range(randomMaxHpRange[0], randomMaxHpRange[1])
 	hp = maxHp
+	# Random Sprite Color of Enemies
+	sprite.modulate = Color(randf(), randf(), randf())
 	pass # Replace with function body.
 	
 func _process(delta):
@@ -34,7 +38,11 @@ func _on_AggroArea_body_exited(body):
 func damage(point):
 	hp -= point
 	updateHealthbar()
-	print((hp / maxHp)*100)
 	
 func updateHealthbar():
 	healthBar.value = (hp / maxHp)*100
+
+func _on_Hitbox_body_entered(body):
+	var damageFunction = get_node("/root/DamageFunction")
+	if "Bullet" in body.name:
+		call_deferred("damage", damageFunction.damageCalculate())
