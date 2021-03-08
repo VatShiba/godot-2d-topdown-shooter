51e6a3cdb6d3a2d21f4ad95b01fc13fc8933e53d
sam <watcharapong.pstst@gmail.com>
2021-03-08 20:32:43 +0700

    [EDIT] Bullet size

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -3,7 +3,8 @@ extends RigidBody2D
 
 func _ready():
 	yield(get_tree().create_timer(1),"timeout")
-	queue_free()
+	if(is_instance_valid(get_tree())):
+		queue_free()
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Enemy"):
