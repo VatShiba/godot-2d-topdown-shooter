90872920d1dc6656853e4b94c61b7bb3788399c0
sam <watcharapong.pstst@gmail.com>
2021-03-11 23:18:48 +0700

    [ADD] New Enemy Scene (Seperate Healthbar and Aggroarea for random scale enemy)

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -6,21 +6,30 @@ var maxHp = null
 var target = null
 var hp = null
 onready var sprite = $Sprite
-onready var healthBar = $HealthBar
+onready var healthBar = get_owner().get_node("HealthBar")
 var floatingText = preload("res://scenes/GUI/FloatingText.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	healthBar.value = 100
+	init()
+
+func init():
+	randomize()
+	# Random Scale
+	var randomScale = rand_range(0.7, 2)
+	self.scale = Vector2(randomScale, randomScale)
+	
+	# Random HP
 	maxHp = rand_range(randomMaxHpRange[0], randomMaxHpRange[1])
 	hp = maxHp
 	# Random Sprite Color of Enemies
 	sprite.modulate = Color(randf(), randf(), randf())
-	
+
 func _physics_process(delta):
 	if target:
 		var motion = global_position.direction_to(target.global_position)
-		sprite.look_at(target.position)
+		look_at(target.position)
 		move_and_slide(motion * speed)
 
 func _on_AggroArea_body_entered(body):
@@ -45,7 +54,7 @@ func onHit(damagePoint):
 		killed()
 		
 func killed():
-	queue_free()
+	get_owner().queue_free()
 	
 func updateHealthbar():
 	healthBar.value = (hp / maxHp)*100
@@ -53,4 +62,6 @@ func updateHealthbar():
 func _on_Hitbox_body_entered(body):
 	var damageFunction = get_node("/root/DamageFunction")
 	if "Bullet" in body.name:
-		call_deferred("onHit", damageFunction.damageCalculate())
+		if(body.isActive):
+			call_deferred("onHit", damageFunction.damageCalculate())
+			body.isActive = false
---
+++ scripts/EnemyOwner.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+onready var enemy = $Enemy
+
+func _physics_process(delta):
+	# Tracking Healthbar on top kineticbody2d
+	$HealthBar.set_global_position(Vector2(enemy.global_position.x - 20 , \
+	enemy.global_position.y - (enemy.get_node("Sprite").texture.get_size().y / 2) - 20 ))
+	
+	# Tracking AggroArea
+	$AggroArea.set_global_position(enemy.global_position)
